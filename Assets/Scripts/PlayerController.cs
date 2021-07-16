06e3354c196ee5cf66a8138a1b8ade17b47c1668
chris <chris@fuzzylojic.com>
2021-07-15 17:44:16 -0700

    playercontroller and some scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float lookSpeed = 2.0f;
+
+    private float yRotation = 0.0f;
+    private float xRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Look();
+    }
+
+    private void Move(){
+        float verticalInput = Input.GetAxis("Vertical"); 
+        float horizontalInput = Input.GetAxis("Horizontal");        
+
+        transform.Translate(Vector3.forward * verticalInput * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+    }
+
+    private void Look(){
+        float horizontalLook = Input.GetAxis("Mouse X"); 
+        float verticalLook = Input.GetAxis("Mouse Y");
+
+        xRotation += verticalLook * lookSpeed;
+        if(xRotation > 90.0f) xRotation = 90.0f;
+        if(xRotation < -90.0f) xRotation = -90.0f;
+        
+        yRotation += horizontalLook * lookSpeed;
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
